0fa3b15ce969682ac7ad42f291a1c51e7cb30d5b
plyte <plyteuthofficial@gmail.com>
2019-06-22 16:00:18 -0700

    Added working files and files from tutorials

---
+++ working-files/setup-dataproc.sh
@@ -0,0 +1,1 @@
+gcloud beta dataproc clusters create cluster-e8db --enable-component-gateway --region us-west1 --subnet default --zone "" --master-machine-type n1-standard-2 --master-boot-disk-size 500 --num-workers 2 --worker-machine-type n1-standard-2 --worker-boot-disk-size 500 --image-version 1.3-debian9 --optional-components ANACONDA,JUPYTER --project root-cortex-244002
