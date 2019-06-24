90126d41a925a8b9ee9c70a845f332d51d550f81
Lynn Langit <lynnlangit@live.com>
2019-06-24 11:59:20 -0700

    added Pig example scripts

--- 12-Use-Spark/README.md
+++ 12-Use-Spark/README.md
@@ -2,3 +2,7 @@
 Companion Repository to Learning Hadoop course on Linked In Learning
 
 Course is here - https://www.lynda.com/Hadoop-tutorials/Hadoop-Fundamentals/191942-2.html
+
+### Sample code
+
+Calculate the digits of Pi using PySpark to create a Spark job
---
+++ 12-Use-Spark/calculatePi.py
@@ -0,0 +1,17 @@
+import pyspark
+import random
+
+if not 'sc' in globals():
+    sc = pyspark.SparkContext()
+
+NUM_SAMPLES = 1000
+
+def sample(p):
+    x,y = random.random(),random.random()
+    return 1 if x*x + y*y < 1 else 0
+
+count = sc.parallelize(xrange(0, NUM_SAMPLES)) \
+            .map(sample) \
+            .reduce(lambda a, b: a + b)
+
+print "Pi is roughly %f" % (4.0 * count / NUM_SAMPLES)
