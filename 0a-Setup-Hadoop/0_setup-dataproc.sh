d06d25241fa561ee6cc7e4da7bd5372333ffadf6
Lynn Langit <lynnlangit@live.com>
2020-01-28 08:23:20 -0800

    Update 0_setup-dataproc.sh

--- 0a-Setup-Hadoop/0_setup-dataproc.sh
+++ 0a-Setup-Hadoop/0_setup-dataproc.sh
@@ -1,6 +1,20 @@
-gcloud beta dataproc clusters create cluster-e8db \
- --enable-component-gateway --region us-west1 --subnet default --zone "" \
- --master-machine-type n1-standard-2 --master-boot-disk-size 500 \
- --num-workers 2 --worker-machine-type n1-standard-2 \
- --worker-boot-disk-size 500 --image-version 1.3-debian9 \
- --optional-components ANACONDA,JUPYTER --project root-cortex-244002
+# put this script all on one line to run
+# this script uses 8 CPUs for your cluster
+# change the following to your settings
+# --region <your-region>
+# --zone <your-zone>
+# --project <your-project>
+
+gcloud beta dataproc clusters create demo 
+    --enable-component-gateway 
+    --region us-central1 
+    --subnet default 
+    --zone us-central1-a 
+    --master-machine-type n1-standard-4 
+    --master-boot-disk-size 500 
+    --num-workers 2 
+    --worker-machine-type n1-standard-2 
+    --worker-boot-disk-size 500 
+    --image-version 1.3-deb9 
+    --optional-components ANACONDA,HIVE_WEBHCAT,JUPYTER,DRUID,PRESTO,ZOOKEEPER 
+    --project nosql-langit
