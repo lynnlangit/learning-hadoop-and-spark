fccf8dbb7ee71fae5810a11ee9b57c2d3db1a059
Lynn Langit <lynnlangit@live.com>
2020-02-03 08:41:33 -0800

    Update calculatePi.py

--- 5-Use-Spark/PySpark/calculatePi.py
+++ 5-Use-Spark/PySpark/calculatePi.py
@@ -10,6 +10,6 @@ def sample(p):
     x,y = random.random(),random.random()
     return 1 if x*x + y*y < 1 else 0
 
-count = sc.parallelize(xrange(0, NUM_SAMPLES)).map(sample).reduce(lambda a, b: a + b)
+count = sc.parallelize(range(0, NUM_SAMPLES)).map(sample).reduce(lambda a, b: a + b)
 
 print ("Pi is roughly %f" % (4.0 * count / NUM_SAMPLES))
